7dbef9e8270a75eb3d712f07be703fcc2af2dc42
Joe <her20053@uvg.edu.gt>
2022-04-01 13:03:33 -0600

    Terminado la funcionalidad de perfiles

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -9,121 +9,116 @@ const Profile = () => {
   let navigate = useNavigate();
   let { username } = useParams();
 
+  const url = window.location.href
+  const arrayTemp = url.split('/')
+  const idUsuario = arrayTemp[arrayTemp.length - 1]
+
+
   const [numeroPerfiles, setNumeroPerfiles] = useState(0);
   const [maxPerfiles, setMaxPerfiles] = useState(0);
-  const [grid, setGrids] = useState({ gridTemplateRows: 'repeat(1, 200px)', gridTemplateColumns: 'repeat(1, 200px)' });
-  const [url, setUrl] = useState(window.location.href);
-  const [arrayTemp, setArrayTemp] = useState(url.split('/'));
-  const [idUsuario, setIdUsuario] = useState(arrayTemp[arrayTemp.length - 1]);
+  const [grid, setGrids] = useState({ gridTemplateRows: '', gridTemplateColumns: '' });
   const [idPlan, setIdPlan] = useState("")
   const [nombrePlanes, setNombrePlanes] = useState([])
 
-  //retrayendo el id del plan
-  // useEffect(() => {
-
-  // },[])
-
-  //retrayendo los perfiles que tiene el usuario
   useEffect(() => {
 
     Axios.post('http://localhost:3001/retraerPlan', {
       id_usuario: idUsuario,
     }).then((response) => {
-      if (response.data[0].id_plan) {
-        setIdPlan(response.data[0].id_plan)
-      }
+      setIdPlan(response.data[0].id_plan)
     });
 
     Axios.post('http://localhost:3001/retraerPerfiles', {
       id_usuario: idUsuario,
     }).then((response) => {
-      // const planesNombreTemp = []
-
       setNombrePlanes(response.data.map(m => m.nombre))
-
-      // response.data.map(p => { planesNombreTemp.push(p.nombre) })
-      // console.log(planesNombreTemp)
-      // console.log(typeof (nombrePlanes))
-      //console.log(response.data)
-      // console.log(nombrePlanes)
-
-     setTimeout(() => {
-      if (nombrePlanes.length > 0) {
-        setNumeroPerfiles(nombrePlanes.length)
-      }
-
-      console.log(nombrePlanes.length)
-
-      if (idPlan === 'pl1') {
-        console.log("plan1")
-        setMaxPerfiles((prev) => { prev = prev + 1 });
-        console.log(maxPerfiles)
-        setGrids({ gridTemplateRows: 'repeat(1, 200px)', gridTemplateColumns: 'repeat(1, 200px)' });
-      } else if (idPlan === 'pl2') {
-        console.log("plan")
-        setMaxPerfiles((prev) => { prev = prev + 4 });
-        console.log(maxPerfiles)
-        setGrids({ gridTemplateRows: 'repeat(1, 200px)', gridTemplateColumns: 'repeat(4 200px)' });
-      } else if (idPlan === 'pl3') {
-        console.log("plan3")
-        setMaxPerfiles((prev) => { prev = prev + 8 });
-        console.log(maxPerfiles)
-        setGrids({ gridTemplateRows: 'repeat(2, 200px)', gridTemplateColumns: 'repeat(4, 200px)' });
-      }
-     }, 2000)
-
     });
   }, [])
 
-  // console.log(nombrePlanes, " console log fuera de useState")
+  useEffect(() => {
+    console.log(idPlan, " test1")
+    if (idPlan === 'pl1') {
+      setMaxPerfiles(1);
+      setGrids({ gridTemplateRows: 'repeat(1, 200px)', gridTemplateColumns: 'repeat(1, 200px)' });
+    }
+    else if (idPlan === 'pl2') {
+      setMaxPerfiles(4);
+      setGrids({ gridTemplateRows: 'repeat(1, 200px)', gridTemplateColumns: 'repeat(4 200px)' });
+    } else if (idPlan === 'pl3') {
+      setMaxPerfiles(8);
+      setGrids({ gridTemplateRows: 'repeat(2, 200px)', gridTemplateColumns: 'repeat(4, 200px)' });
+    }
+  },
+    [idPlan])
 
-  //seteando el contador igual al numero de perfiles del
-  // useEffect(() => { 
+  useEffect(() => {
+    console.log(nombrePlanes, " test2")
+    setNumeroPerfiles(nombrePlanes.length)
 
-  // }, [])
+  },
+    [nombrePlanes])
 
-  //if para ver cuantos perfiles puede tener el usuario
-  // useEffect(() => {
+  useEffect(() => {
+    console.log(numeroPerfiles, " test3")
+  },
+    [numeroPerfiles])
 
-  // }, [])
+  useEffect(() => {
+    console.log(maxPerfiles, " test4")
+  },
+    [maxPerfiles])
+
+  useEffect(() => {
+    console.log(grid.gridTemplateRows, grid.gridTemplateColumns, " test5")
+  },
+    [grid])
+
+
+
+
+
+  console.log("---------------------RENDER----------------------")
+  console.log(numeroPerfiles == maxPerfiles)
 
 
   return (
     <div className='Profile'>
       <h1 onClick={() => { navigate("/") }} id="titulo">MEMEFLIX</h1>
       <h1 id='quien'>¿Quién Está Viendo?</h1>
-      <div className='gridPerfiles' style={{ gridTemplateRows: grid.gridTemplateRows, gridTemplateColumns: grid.gridTemplateColumns }}>
-
-        {/* <h1 style={{ color: 'white' }}>{typeof (nombrePlanes)}</h1> */}
-
-        {/* {nombrePlanes.objects.forEach(n => {
-          return (
-            <div>
-              <TagFaces style={{ color: "white", fontSize: '180' }} className="perfil" />
-              <h4>{n}</h4>
-            </div>
-          )
-        })} */}
+      <div className='gridPerfiles' style={{
+        gridTemplateRows: grid.gridTemplateRows,
+        gridTemplateColumns: grid.gridTemplateColumns
+      }}>
 
         {
           nombrePlanes.map(nombre => {
             return (
               <div key={nombre}>
-                <TagFaces onClick={() => { navigate(`/home/${idUsuario}/${nombre}`) }} style={{ color: "white", fontSize: '180' }} className="perfil" />
+                <TagFaces onClick={() => {
+                  navigate(`/home/${idUsuario}/${nombre}`)
+                }}
+                  style={{ color: "white", fontSize: '180' }}
+                  className="perfil" />
                 <h4>{nombre}</h4>
               </div>
             )
           })
         }
-        {numeroPerfiles == maxPerfiles ? 
-          () => {return 
-          <div>
-            <AddCircle onClick={() => { navigate(`/addprofile/${idUsuario}`) }} style={{ color: "white", fontSize: '180' }} id='agregarPerfil' />
-            <h4>Agregar Perfil</h4>
-          </div>}
-          : <h1></h1>
+
+        {
+          // Ver si renderizamos el boton de mas
+          (numeroPerfiles == maxPerfiles)
+            ?
+            <p></p>
+            :
+            <div>
+              <AddCircle onClick={() => { navigate(`/addprofile/${idUsuario}`) }} style={{ color: "white", fontSize: '180' }} id='agregarPerfil' />
+              <h4>Agregar Perfil</h4>
+            </div>
         }
-        
+
+
+
       </div>
       <div className="Manejar">
         <button id="btnmanejar">Manejar Perfiles</button>
